75f042d7768d842746ac19ec5028f6804ca9a1af
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2021-10-02 13:04:54 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,52 @@
+let listItems = [
+    {
+        isbn: "1600506460320",
+        title: "Great book",
+    },
+    {
+        isbn: "1600506460373",
+        title: "Ok book",
+    },
+    {
+        isbn: "1600506460521",
+        title: "Bad book",
+    },
+    {
+        isbn: "1600506460456",
+        title: "Terrible book",
+    },
+];
+
+createList();
+
+function createList() {
+    const listContainer = document.querySelector("ul");
+
+    listContainer.innerHTML = "";
+
+    listItems.forEach(function (item) {
+        listContainer.innerHTML += `<li><span>${item.isbn}</span> <span>${item.title}</span> <i class="fa fa-trash" data-item="${item}"></li</li>`;
+    });
+
+    const trashCans = document.querySelectorAll("li i");
+
+    trashCans.forEach(function (can) {
+        can.addEventListener("click", removeFromList);
+    });
+}
+
+function removeFromList() {
+    console.log(event);
+
+    const deleteThisItem = event.target.dataset.item;
+
+    const newList = listItems.filter(function (item) {
+        if (deleteThisItem !== item) {
+            return true;
+        }
+    });
+
+    listItems = newList;
+
+    createList();
+}
